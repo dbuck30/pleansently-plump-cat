10025a33f3e5dae0580425b868cfa2ec399e0f6c
dbuck30 <davidnbuck@ksu.edu>
2017-06-04 15:46:15 -0500

    uploaded physicstextbookproblems file
    Putting physics problems into programs.

---
+++ PhysicsTextbookProblems/Untitled2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main()
+{
+	int x = 39;
+	int i = 2;
+	int j = 1;
+	int n = (i/j * i/j * x);
+	cout << "The answer is " << n << endl;
+	return 0;
+	
+}
